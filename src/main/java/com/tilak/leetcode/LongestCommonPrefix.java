b2422f71bac8a04e577ea1d35e9c27c047f62046
Tilak <tpatel9912@gmail.com>
2020-08-06 16:57:11 -0500

    LongestCommonPrefix in string

---
+++ src/main/java/com/tilak/leetcode/LongestCommonPrefix.java
@@ -0,0 +1,31 @@
+package com.tilak.leetcode;
+
+public class LongestCommonPrefix {
+
+	public static void main(String[] args) {
+		String[] strArray = { "flower", "flow", "flute" };
+		System.out.println("Prefix is = "+findLongestPrefix(strArray));
+		
+//		System.out.println("flowsdfjdjd".indexOf("flower"));  o/p -> -1
+//		System.out.println("flowjfldjlkfd".indexOf("flowe")); o/p -> -1
+//		System.out.println("flowljfldkjl".indexOf("flow"));   o/p ->  1
+
+	}
+
+	private static String findLongestPrefix(String[] strArray) {
+
+		String prefix = strArray[0];
+		
+		//start iterating form second element and check if it has prefix
+		for (int i=1; i<strArray.length; i++) {
+			
+			//remove teh prefix result until it finds same prefix with array element  
+			while (strArray[i].indexOf(prefix) != 0) {
+				prefix = prefix.substring(0, prefix.length() -1);
+				//System.out.println("Prefix ="+prefix);
+			}
+		}
+		return prefix;
+	}
+
+}
